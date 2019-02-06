d4ba356814fcda6e41a9f6950e2e5100ccfde74f
WilliamDaniel2406 <williamquispe2406@gmail.com>
2019-02-06 13:15:12 -0500

    Add SplashScreen

---
+++ app/src/main/java/com/wallpaper/rickandmorty/SplashActivity.kt
@@ -0,0 +1,31 @@
+package com.wallpaper.rickandmorty
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.WindowManager
+import java.lang.Exception
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN)
+        setContentView(R.layout.activity_splashscreen)
+
+        val background = object : Thread(){
+           override fun run(){
+                try {
+                    Thread.sleep(3000)
+
+                    val intent = Intent(baseContext,MainActivity::class.java)
+                    startActivity(intent)
+                }catch (e: Exception){
+                    e.printStackTrace()
+                }
+            }
+        }
+        background.start()
+    }
+}
